10aa8831845bb7d032a0e36b87031ca08042e202
Rafaelaugusto16 <meu_nome@email.com>
2023-03-06 22:00:12 -0300

    adding value limiter

--- Guess_number.py
+++ Guess_number.py
@@ -3,22 +3,25 @@ print("Bem vindo ao jogo de adivinhação!")
 print("********************************")
 numero_secreto = 13
 total_de_rodadas= 3
-
-for rodada in range (1,total_de_rodadas + 1):
-    print("Número de tentativas", rodada, "de", total_de_rodadas)
-    chute = int(input("Entre com um número: "))
+rodada= 1
+while (rodada <= total_de_rodadas):
+    print("Número de tentativas {} de {}".format(rodada, total_de_rodadas))
+    chute = int(input("Entre com um número de 1 a 20: "))
     print("Você digitou o número:", chute)
-
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
-    if  (acertou):
-        print("Você acertou!")
-        break
-    else:
-        if(maior):
-            print("Você errou! Seu número é maior que o número secreto")
-        elif(menor):
-            print("Você errou! Seu número é menor que o número secreto")
-    rodada = rodada + 1
+    if(chute<1 or chute>20):
+        print("O número deve estar entre 1 e 20!")
+        continue
+    elif(chute>=1 and chute<=20):
+        rodada = rodada + 1
+        if  (acertou):
+           print("Você acertou!")
+           break
+        else:
+            if(maior):
+                print("Você errou! Seu número é maior que o número secreto.")
+            elif(menor):
+                print("Você errou! Seu número é menor que o número secreto.")
 print("Fim do jogo!")
