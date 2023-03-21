65a87ec0db3000791f75c296a298de1bbef6457a
Rafaelaugusto16 <meu_nome@email.com>
2023-03-20 23:12:39 -0300

    organizing game selection

--- Select_game.py
+++ Select_game.py
@@ -1,12 +1,18 @@
 import Guess_number
 import Guess_word
-print("Escolha o jogo que deseja jogar")
-jogo = int(input("Adivinhe o número [1] ou Adivinhe a palavra [2]:"))
-if (jogo == 1):
-    print("Você escolheu Adivinhe o número! Boa sorte!")
-    Guess_number.play()
-elif(jogo == 2):
-    print("Você escolheu Adivinhe a palavra! Boa sorte!")
-    Guess_word.play()
-else:
-    print("escolha um jogo conforme indicado!")
+import Select_game
+
+
+def play():
+    print("Escolha o jogo que deseja jogar")
+    jogo = int(input("Adivinhe o número [1] ou Adivinhe a palavra [2]:"))
+    if (jogo == 1):
+        print("Você escolheu Adivinhe o número! Boa sorte!")
+        Guess_number.play()
+    elif(jogo == 2):
+        print("Você escolheu Adivinhe a palavra! Boa sorte!")
+        Guess_word.play()
+    else:
+        print("escolha um jogo conforme indicado!")
+if (__name__ == ("__main__")):
+    Select_game.play()
