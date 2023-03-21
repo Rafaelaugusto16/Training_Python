a1ec768801f92262cb976a9630824f7bed107c56
Rafaelaugusto16 <meu_nome@email.com>
2023-03-20 23:08:57 -0300

    creating looping with true and false

--- Guess_word.py
+++ Guess_word.py
@@ -9,6 +9,20 @@ def play():
     print("********************************")
     print("Adivinhe a palavra secreta!")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Chute uma letra:")
+
+        posição_letra = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Você acertou a letra! {} está na posição {} " .format(chute,posição_letra))
+            posição_letra = posição_letra +1
+
 if (__name__ == ("__main__")):
     Guess_word.play()
 
