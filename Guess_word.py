0feb97d736672beabb54f221219b3f8273807667
Rafaelaugusto16 <meu_nome@email.com>
2023-04-12 01:20:07 -0300

    organizing code and implementing designs

--- Guess_word.py
+++ Guess_word.py
@@ -5,51 +5,160 @@ def play():
     mensagem_abertura()
     palavra_secreta = carrega_palavra_secreta()
     numero_letras = tamanho_palavra(palavra_secreta)
-    tentativas = 6
+    print(" ".join(numero_letras))
+
     erro = 0
+
     enforcou = False
     acertou = False
 
-    print(" ".join(numero_letras))
+
     while (not enforcou and not acertou):
-        chute = input("Chute uma letra:")
-        chute = chute.strip().casefold()
-        indice = 0
-
+        chute = chute_do_jogador()
         if (chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    numero_letras [indice] = letra
-                indice += 1
-
+            acertou_a_letra(chute, numero_letras, palavra_secreta)
         else:
-            tentativas -= 1
-            print("Você errou! Restam apenas {} tentativas" .format(tentativas))
             erro += 1
+            desenha_forca(erro)
 
         print(" ".join(numero_letras))
-        enforcou = erro == 6
+        enforcou = erro == 7
         acertou = "_" not in numero_letras
     if (acertou):
-        print("Você acertou a palavra secreta!")
+        imprime_mensagem_vencedor()
     else:
-        print("Suas tentativas acabaram! A palavra secreta era '{}'." .format(palavra_secreta))
+        imprime_mensagem_perdedor(palavra_secreta)
+
     print("Fim do jogo!")
 def mensagem_abertura():
-    print("********************************")
+    print("*********************************")
     print("Bem vindo ao jogo de adivinhação!")
-    print("********************************")
+    print("*********************************")
     print("Adivinhe a palavra secreta!")
 def carrega_palavra_secreta():
-    with open ('palavras.txt') as arquivo:
-        palavras = []
-        for linha in arquivo:
-            linha = linha.strip()
-            palavras.append(linha)
-    palavra_secreta = random.choice(palavras)
+    tema = False
+    while (tema == False):
+        try:
+            tema_palavra = int(input("Escolha qual o tema da rodada [1] frutas [2] estados:"))
+            if (tema_palavra == 1):
+                print("O tema escolhido foi frutas! Boa sorte!")
+                with open ('palavras_frutas.txt',encoding='UTF-8') as arquivo:
+                    palavras = []
+                    for linha in arquivo:
+                        linha = linha.strip()
+                        palavras.append(linha)
+                tema = True
+                palavra_secreta = random.choice(palavras).casefold()
+                palavra_secreta = "".join(palavra_secreta.split())
+            elif (tema_palavra == 2):
+                with open('Palavras_estados.txt', encoding='UTF-8') as arquivo:
+                    palavras = []
+                    for linha in arquivo:
+                        linha = linha.strip()
+                        palavras.append(linha)
+                tema = True
+                palavra_secreta = random.choice(palavras).casefold()
+                palavra_secreta = "".join(palavra_secreta.split())
+            else:
+                print("Escolha o tema conforme indicado!")
+        except ValueError:
+                print("Escolha o tema conforme indicado!")
     return palavra_secreta
 def tamanho_palavra(palavra_secreta):
     return ["_" for letra in palavra_secreta]
+def chute_do_jogador():
+    chute = input("Chute uma letra:")
+    chute = chute.strip().casefold()
+    return chute
+def acertou_a_letra(chute, numero_letras, palavra_secreta):
+    indice = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            numero_letras[indice] = letra
+        indice += 1
+
+def imprime_mensagem_vencedor():
+    print("Você acertou a palavra secreta!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("*"*55)
+    print("Suas tentativas acabaram! A palavra secreta era '{}'.".format(palavra_secreta))
+    print("*" *55)
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def desenha_forca(erro):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erro == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erro == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erro == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erro == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erro == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erro == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erro == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 if (__name__ == ("__main__")):
     Guess_word.play()
